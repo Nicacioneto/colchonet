a40bd7bf039e77d1020d32ba0bef70bb95700ac8
Nicacioneto <nicacionetobsb@hotmail.com>
2016-03-23 12:08:20 -0300

    Implements the control of  mass-assignment, create the update page, and show the errors of registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,41 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+	def new
+		@user = User.new
+	end
 
-  def create
-  	@user = User.new(params[:user])
-  	if @user.save
-  		redirect_to @user,
-  		:notice => 'Cadastro criado com sucesso!'
-  	else 
-  		render:new
-  	end	
-  end
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user, :notice => 'Cadastro criado com sucesso!'
+		else
+			render :new
+		end
+
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+		else
+			render :update
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:bio, :email, :full_name, :location,:password, :password_confirmation)
+	end
 end
+
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   # Cuidado para não esquecer a vírgula no final da linha.
-  attr_accessible :bio, :email, :full_name, :location,
-  :password, :password_confirmation
   validates_presence_of :email, :full_name, :location, :password
   validates_confirmation_of :password
   validates_length_of :bio, :minimum => 30, :allow_blank => false
