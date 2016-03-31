1ad01e2fc954531c7da334aedc1331d15c452941
Nicacioneto <nicacionetobsb@hotmail.com>
2016-03-31 10:21:17 -0300

    Add helpers and validate the forms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ActiveRecord::Base
+  validates_presence_of :title, :location, :description
+
+  validates_length_of :description, :minimum => 5, :allow_blank => false
+
   def complete_name
     "#{title}, #{location}"
   end
